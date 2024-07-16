dbe28b4f1aeb4d952f32a7866561ea16a5c39f37
Filip Lazovic <lazovicff@gmail.com>
2024-07-16 18:53:52 +0200

    JSON deserialise instead of rls

--- common/src/db.rs
+++ common/src/db.rs
@@ -1,6 +1,5 @@
-use alloy_rlp::Decodable;
 use rocksdb::{Error as RocksDBError, IteratorMode, DB};
-use serde::Serialize;
+use serde::{de::DeserializeOwned, Serialize};
 use serde_json::{to_vec, Error as SerdeError};
 use std::error::Error as StdError;
 use std::fmt::{Display, Formatter, Result as FmtResult};
@@ -43,11 +42,11 @@ impl Db {
 		self.connection.put(key, value).map_err(|e| DbError::RocksDB(e))
 	}
 
-	pub fn read_from_start<I: DbItem + Decodable>(&self, num_elements: usize) -> Vec<I> {
+	pub fn read_from_start<I: DbItem + DeserializeOwned>(&self, num_elements: usize) -> Vec<I> {
 		let iter = self.connection.iterator(IteratorMode::Start);
 		let mut elements = Vec::new();
 		for (_, (_, db_value)) in iter.map(Result::unwrap).enumerate().take(num_elements) {
-			let tx = I::decode(&mut db_value.as_ref()).unwrap();
+			let tx = serde_json::from_slice(db_value.as_ref()).unwrap();
 			elements.push(tx);
 		}
 		elements
