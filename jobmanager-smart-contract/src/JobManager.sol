80b2d44b7245ca3ec4edfbd3862ed27c840a1bb9
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2024-09-07 00:19:43 +0800

    feat: add helper functions for signature check

--- jobmanager-smart-contract/src/JobManager.sol
+++ jobmanager-smart-contract/src/JobManager.sol
@@ -55,4 +55,27 @@ contract JobManager {
             verifiers[_verifiers[i]] = true;
         }
     }
+
+    // Recover signer from the provided hash and signature
+    function recoverSigner(bytes32 hash, bytes memory signature) internal pure returns (address) {
+        bytes32 messageHash = prefixed(hash);
+        (uint8 v, bytes32 r, bytes32 s) = splitSignature(signature);
+        return ecrecover(messageHash, v, r, s);
+    }
+
+    // Helper function to prefix the hash with "\x19Ethereum Signed Message:\n32" to match with the standard
+    function prefixed(bytes32 hash) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
+    }
+
+    // Helper function to split the signature into `r`, `s`, and `v`
+    function splitSignature(bytes memory sig) internal pure returns (uint8 v, bytes32 r, bytes32 s) {
+        require(sig.length == 65, "Invalid signature length");
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := byte(0, mload(add(sig, 96)))
+        }
+        return (v, r, s);
+    }
 }
