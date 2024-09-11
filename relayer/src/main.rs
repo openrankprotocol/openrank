874e1c98f35df12c49a25719bb4abe42af0b06d8
jenya <hypnagonia>
2024-09-11 12:04:25 +0200

    initial struct

---
+++ relayer/src/main.rs
@@ -0,0 +1,9 @@
+use openrank_verifier::{self, VerifierNode};
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mut vn = VerifierNode::init().await?;
+    vn.node_recovery()?;
+    vn.run().await
+}
