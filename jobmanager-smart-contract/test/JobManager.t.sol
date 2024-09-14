d3d73352fe526f4d1f29cd36d5080945027f5d6b
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2024-09-14 18:04:39 +0800

    test: reduce the number of test wallets

--- jobmanager-smart-contract/test/JobManager.t.sol
+++ jobmanager-smart-contract/test/JobManager.t.sol
@@ -7,48 +7,24 @@ import {JobManager} from "../src/JobManager.sol";
 contract JobManagerTest is Test {
     JobManager public jobManager;
     
-    address public blockBuilder1;
-    address public blockBuilder2;
-    address public blockBuilder3;
-    
-    address public computer1;
-    address public computer2;
-    address public computer3;
-
-    address public verifier1;
-    address public verifier2;
-    address public verifier3;
-
+    address public blockBuilder;    
+    address public computer;
+    address public verifier;
 
     function setUp() public {
         // Get the addresses from the default test wallets
-        blockBuilder1 = vm.addr(1);
-        blockBuilder2 = vm.addr(2);
-        blockBuilder3 = vm.addr(3);
+        blockBuilder = vm.addr(1);
+        address[] memory _blockBuilders = new address[](1);
+        _blockBuilders[0] = blockBuilder;
 
-        address[] memory _blockBuilders = new address[](3);
-        _blockBuilders[0] = blockBuilder1;
-        _blockBuilders[1] = blockBuilder2;
-        _blockBuilders[2] = blockBuilder3;
+        computer = vm.addr(2);
+        address[] memory _computers = new address[](1);
+        _computers[0] = computer;
 
-        computer1 = vm.addr(4);
-        computer2 = vm.addr(5);
-        computer3 = vm.addr(6);
-
-        address[] memory _computers = new address[](3);
-        _computers[0] = computer1;
-        _computers[1] = computer2;
-        _computers[2] = computer3;
-
-        verifier1 = vm.addr(7);
-        verifier2 = vm.addr(8);
-        verifier3 = vm.addr(9);
-
-        address[] memory _verifiers = new address[](3);
-        _verifiers[0] = verifier1;
-        _verifiers[1] = verifier2;
-        _verifiers[2] = verifier3;
-
+        verifier = vm.addr(3);
+        address[] memory _verifiers = new address[](1);
+        _verifiers[0] = verifier;
+        
         jobManager = new JobManager(_blockBuilders, _computers, _verifiers);
     }
 
