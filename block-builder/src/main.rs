e0132d5fb792cc506de8842a32e32be4b0791fe5
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2024-09-23 13:56:03 +0800

    feat: add smart contract client logic in block-builder node

--- block-builder/src/main.rs
+++ block-builder/src/main.rs
@@ -1,11 +1,17 @@
 use openrank_block_builder::{self, BlockBuilderNode};
 use openrank_sequencer::{self, SequencerNode};
+use smart_contract_client::JobManagerClient;
 use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let mut bb = BlockBuilderNode::init().await?;
     let mut sequencer = SequencerNode::init().await?;
+    let smc = JobManagerClient::new(
+        "",
+        "",
+        "",
+    );
     let block_builder_task = tokio::spawn(async move {
         let res = bb.run().await;
         if let Err(e) = res {
@@ -20,6 +26,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
             std::process::exit(1);
         }
     });
-    let _ = tokio::join!(block_builder_task, sequencer_task);
+    let smart_contract_client_task = tokio::spawn(async move {
+        let res = smc.run().await;
+        if let Err(e) = res {
+            eprintln!("{}", e);
+            std::process::exit(1);
+        }
+    });
+    let _ = tokio::join!(block_builder_task, sequencer_task, smart_contract_client_task);
     Ok(())
 }
