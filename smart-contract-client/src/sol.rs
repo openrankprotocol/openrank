5dcd7dcf5636e5ee47f8d23725a6c53baa97fc18
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2024-10-29 20:33:05 +0800

    ref: remove smart contract & client pkgs

--- smart-contract-client/src/sol.rs
+++
@@ -1,9 +0,0 @@
-use alloy::sol;
-
-// Codegen from ABI file to interact with the contract.
-sol!(
-    #[allow(missing_docs)]
-    #[sol(rpc)]
-    ComputeManager,
-    "./abi/ComputeManager.json"
-);
