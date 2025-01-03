46f8d155771a4697dbbf565be294d83f48a960a9
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2025-01-03 14:00:25 +0800

    feat: impl "BaseRunner::new()" func

--- common/src/runners/mod.rs
+++ common/src/runners/mod.rs
@@ -3,7 +3,7 @@ use crate::{
         fixed::DenseMerkleTree, incremental::DenseIncrementalMerkleTree,
     },
     misc::OutboundLocalTrust,
-    topics::DomainHash,
+    topics::{Domain, DomainHash},
     tx::{
         trust::OwnedNamespace,
         TxHash,
@@ -26,5 +26,43 @@ pub struct BaseRunner {
     lt_sub_trees: HashMap<DomainHash, HashMap<u64, DenseIncrementalMerkleTree<Keccak256>>>,
     lt_master_tree: HashMap<DomainHash, DenseIncrementalMerkleTree<Keccak256>>,
     st_master_tree: HashMap<DomainHash, DenseIncrementalMerkleTree<Keccak256>>,
-    compute_tree: HashMap<DomainHash, HashMap<TxHash, DenseMerkleTree<Keccak256>>>,
+}
+
+impl BaseRunner {
+    pub fn new(domains: &[Domain]) -> Self {
+        let mut count = HashMap::new();
+        let mut indices = HashMap::new();
+        let mut local_trust = HashMap::new();
+        let mut seed_trust = HashMap::new();
+        let mut lt_sub_trees = HashMap::new();
+        let mut lt_master_tree = HashMap::new();
+        let mut st_master_tree = HashMap::new();
+        let mut compute_results = HashMap::new();
+        for domain in domains {
+            let domain_hash = domain.to_hash();
+            count.insert(domain_hash, 0);
+            indices.insert(domain_hash, HashMap::new());
+            local_trust.insert(domain.trust_namespace(), HashMap::new());
+            seed_trust.insert(domain.trust_namespace(), HashMap::new());
+            lt_sub_trees.insert(domain_hash, HashMap::new());
+            lt_master_tree.insert(
+                domain_hash,
+                DenseIncrementalMerkleTree::<Keccak256>::new(32),
+            );
+            st_master_tree.insert(
+                domain_hash,
+                DenseIncrementalMerkleTree::<Keccak256>::new(32),
+            );
+            compute_results.insert(domain_hash, Vec::<f32>::new());
+        }
+        Self {
+            count,
+            indices,
+            local_trust,
+            seed_trust,
+            lt_sub_trees,
+            lt_master_tree,
+            st_master_tree,
+        }
+    }
 }
