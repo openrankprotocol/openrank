13298399d625e3a661fc841cd4c98791c3efe14a
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2025-01-06 20:24:19 +0800

    ref: update visibility of "BaseRunner" fields

--- common/src/runners/mod.rs
+++ common/src/runners/mod.rs
@@ -17,14 +17,13 @@ pub mod verification_runner;
 #[derive(Getters)]
 #[getset(get = "pub")]
 pub(crate) struct BaseRunner {
-    pub(crate) count: HashMap<DomainHash, u64>,
-    pub(crate) indices: HashMap<DomainHash, HashMap<String, u64>>,
-    pub(crate) local_trust: HashMap<OwnedNamespace, HashMap<u64, OutboundLocalTrust>>,
-    pub(crate) seed_trust: HashMap<OwnedNamespace, HashMap<u64, f32>>,
-    pub(crate) lt_sub_trees:
-        HashMap<DomainHash, HashMap<u64, DenseIncrementalMerkleTree<Keccak256>>>,
-    pub(crate) lt_master_tree: HashMap<DomainHash, DenseIncrementalMerkleTree<Keccak256>>,
-    pub(crate) st_master_tree: HashMap<DomainHash, DenseIncrementalMerkleTree<Keccak256>>,
+    count: HashMap<DomainHash, u64>,
+    indices: HashMap<DomainHash, HashMap<String, u64>>,
+    local_trust: HashMap<OwnedNamespace, HashMap<u64, OutboundLocalTrust>>,
+    seed_trust: HashMap<OwnedNamespace, HashMap<u64, f32>>,
+    lt_sub_trees: HashMap<DomainHash, HashMap<u64, DenseIncrementalMerkleTree<Keccak256>>>,
+    lt_master_tree: HashMap<DomainHash, DenseIncrementalMerkleTree<Keccak256>>,
+    st_master_tree: HashMap<DomainHash, DenseIncrementalMerkleTree<Keccak256>>,
 }
 
 impl BaseRunner {
